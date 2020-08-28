635680d28330d3451ed0ff2702bf05b3e4868258
Dustin <dtc97bh@gmail.com>
2020-08-27 22:52:22 -0600

    Adding a basic Maven structure, as well as barebones project outline

---
+++ src/main/java/team/moxie/App.java
@@ -0,0 +1,8 @@
+package team.moxie;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! This is the main class that is built by Maven." );
+    }
+}
