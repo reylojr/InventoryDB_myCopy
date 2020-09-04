dce7ae11f5f0001e22f49651dab2591d4e9a6209
Cody <cmh.howard1@gmail.com>
2020-09-04 08:58:06 -0600

    UI update

--- src/main/java/team/moxie/App.java
+++ src/main/java/team/moxie/App.java
@@ -1,13 +1,16 @@
 package team.moxie;
 
 import java.sql.*;
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
 import java.sql.SQLException;
 
 public class App {
 
 	public static void main(String[] args) {
 		try {
-			DbDriver driver = new DbDriver("192.168.1.10", "3306", "inv", "dustin", "Hellgirlfan97@");
+			DbDriver driver = new DbDriver("50.116.26.153", "3306", "inv", "team", "GJ&8YahAh%kS#i");
 			System.out.println("Connection successful.");
 			System.out.println("Running tests!");
 
@@ -52,6 +55,29 @@ public class App {
 			System.out.println();
 
 			System.out.println("Tests have completed.");
+
+			//Added by Shawn, Cody - option 1
+			Scanner sysIn = new Scanner(System.in);
+			System.out.println("Database Manager.\nPress 1 for operations management.\nPress 2 for customer service");
+			int test = sysIn.nextInt();
+			switch (test) {
+				case 1:
+					//operations();
+				case 2:
+					//customer();
+					Scanner cusIn = new Scanner(System.in);
+					int cusSel = sysIn.nextInt();
+					System.out.println("Welcome to database customer service. Please make selection:\n");
+					System.out.println("1: Search inventory\n2: Make purchase");
+					switch (cusSel) {
+						case (1):
+							System.out.println("Enter product ID");
+							String ID = sysIn.nextLine();
+							System.out.println(driver.searchById(ID));
+					}
+			}
+
+
 		} catch (SQLException throwables) {
 			throwables.printStackTrace();
 		}
